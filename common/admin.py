f46bae1bcaa9a4cfe0f82355715f58540d775321
abduqulovabdulla3108@gmail.com <abduqulovabdulla3108@gmail.com>
2024-01-27 14:51:37 +0500

    created admin

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Region, District, Quarter
+
+
+class DistrictInline(admin.TabularInline):
+    model = District
+    extra = 1
+
+
+class QuarterInline(admin.TabularInline):
+    model = Quarter
+    extra = 1
+
+
+class RegionAdmin(admin.ModelAdmin):
+    inlines = [DistrictInline]
+    list_display = ('name', )
+    search_fields = ('name', )
+
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region')
+    list_filter = ('region', )
+    search_fields = ('name', )
+    inlines = [QuarterInline]
+
+
+class QuarterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'district')
+    list_filter = ('district', )
+    search_fields = ('name', )
+
+
+admin.site.register(Region, RegionAdmin)
+admin.site.register(District, DistrictAdmin)
+admin.site.register(Quarter, QuarterAdmin)

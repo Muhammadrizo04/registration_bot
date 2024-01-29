e182acd4a6438bbf94f6b779d8e9ebbcc4fa1d0f
Muhammadrizo04 <muhammadrizomuxtorov389@gmail.com>
2024-01-29 18:06:03 +0500

    added BotUsers model

--- botapp/models.py
+++ botapp/models.py
@@ -1,6 +1,21 @@
 from django.db import models
-from common.models import BaseModel
+from common.models import BaseModel, Region, District, Quarter
 
 
-class Category(BaseModel):
+class Direction(BaseModel):
     pass
+
+
+class BotUsers(BaseModel):
+    chat_id = models.IntegerField()
+    nickname = models.CharField(max_length=120)
+    user_lang = models.CharField(max_length=3)
+    phone = models.CharField(max_length=15)
+    full_name = models.CharField(max_length=80)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE(), related_name="bot_users")
+    district = models.ForeignKey(District, on_delete=models.CASCADE(), related_name="bot_users")
+    quarter = models.ForeignKey(Quarter, on_delete=models.CASCADE(), related_name="bot_users")
+    adress = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.full_name
